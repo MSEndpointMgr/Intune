440e7b72db5dc838d90006509fd6ee929f69f628
Nickolaj Andersen <nickolaj@outlook.com>
2020-01-20 12:05:36 +0100

    IntuneWin32App version 1.0.1

--- Modules/IntuneWin32App/IntuneWin32App.psm1
+++ Modules/IntuneWin32App/IntuneWin32App.psm1
@@ -270,10 +270,11 @@ function Get-IntuneWin32App {
         Author:      Nickolaj Andersen
         Contact:     @NickolajA
         Created:     2020-01-04
-        Updated:     2020-01-04
+        Updated:     2020-01-20
 
         Version history:
         1.0.0 - (2020-01-04) Function created
+        1.0.1 - (2020-01-20) Updated to load all properties for objects return and support multiple objects returned for wildcard search when specifying display name
     #>
     [CmdletBinding(SupportsShouldProcess = $true, DefaultParameterSetName = "Default")]
     param(
@@ -309,41 +310,61 @@ function Get-IntuneWin32App {
         Get-AuthToken -TenantName $TenantName -ApplicationID $ApplicationID -PromptBehavior $PromptBehavior
     }
     Process {
-        Write-Verbose -Message "Attempting to retrieve all mobileApps resources"
-        $MobileApps = Invoke-IntuneGraphRequest -APIVersion "Beta" -Resource "mobileApps" -Method "GET"
-        if ($MobileApps.value.Count -ge 1) {
-            Write-Verbose -Message "Filtering query response for mobileApps matching type '#microsoft.graph.win32LobApp'"
-            $Win32MobileApps = $MobileApps.value | Where-Object { $_.'@odata.type' -like "#microsoft.graph.win32LobApp" }
-            if ($Win32MobileApps -ne $null) {
-                switch ($PSCmdlet.ParameterSetName) {
-                    "DisplayName" {
-                        Write-Verbose -Message "Querying for Win32 apps matching displayName: $($DisplayName)"
-                        $Win32App = $Win32MobileApps | Where-Object { $_.displayName -like "*$($DisplayName)*" }
+        switch ($PSCmdlet.ParameterSetName) {
+            "DisplayName" {
+                Write-Verbose -Message "Attempting to retrieve all mobileApps resources to determine ID of Win32 app"
+                $Win32AppList = New-Object -TypeName System.Collections.ArrayList
+                $MobileApps = Invoke-IntuneGraphRequest -APIVersion "Beta" -Resource "mobileApps" -Method "GET"
+                if ($MobileApps.value.Count -ge 1) {
+                    Write-Verbose -Message "Filtering query response for mobileApps matching type '#microsoft.graph.win32LobApp'"
+                    $Win32MobileApps = $MobileApps.value | Where-Object { $_.'@odata.type' -like "#microsoft.graph.win32LobApp" }
+                    if ($Win32MobileApps -ne $null) {
+                        Write-Verbose -Message "Filtering for Win32 apps matching displayName: $($DisplayName)"
+                        $Win32MobileApps = $Win32MobileApps | Where-Object { $_.displayName -like "*$($DisplayName)*" }
+                        if ($Win32MobileApps -ne $null) {
+                            foreach ($Win32MobileApp in $Win32MobileApps) {
+                                Write-Verbose -Message "Querying for Win32 app using ID: $($Win32MobileApp.id)"
+                                $Win32App = Invoke-IntuneGraphRequest -APIVersion "Beta" -Resource "mobileApps/$($Win32MobileApp.id)" -Method "GET"
+                                $Win32AppList.Add($Win32App) | Out-Null
+                            }
+
+                            # Handle return value
+                            return $Win32AppList
+                        }
+                        else {
+                            Write-Warning -Message "Query for Win32 app returned an empty result, no apps matching the specified search criteria was found"
+                        }
                     }
-                    "ID" {
-                        Write-Verbose -Message "Querying for Win32 apps matching id: $($ID)"
-                        $Win32App = $Win32MobileApps | Where-Object { $_.id -like $ID }
-                    }
-                    default {
-                        Write-Verbose -Message "Querying for all Win32 apps"
-                        $Win32App = $Win32MobileApps
+                    else {
+                        Write-Warning -Message "Query for Win32 apps returned an empty result, no apps matching type 'win32LobApp' was found in tenant"
                     }
                 }
-    
+            }
+            "ID" {
+                Write-Verbose -Message "Querying for Win32 apps matching id: $($ID)"
+                $Win32App = Invoke-IntuneGraphRequest -APIVersion "Beta" -Resource "mobileApps/$($ID)" -Method "GET"
+
                 # Handle return value
-                if ($Win32App -ne $null) {
-                    return $Win32App
+                return $Win32App
+            }
+            default {
+                Write-Verbose -Message "Querying for all Win32 apps"
+                $Win32AppList = New-Object -TypeName System.Collections.ArrayList
+                $Win32MobileApps = (Invoke-IntuneGraphRequest -APIVersion "Beta" -Resource "mobileApps?`$filter=isof('microsoft.graph.win32LobApp')" -Method "GET").value
+                if ($Win32MobileApps.Count -ge 1) {
+                    foreach ($Win32MobileApp in $Win32MobileApps) {
+                        Write-Verbose -Message "Querying explicitly to retrieve all properties for Win32 app with ID: $($Win32MobileApp.id)"
+                        $Win32App = Invoke-IntuneGraphRequest -APIVersion "Beta" -Resource "mobileApps/$($Win32MobileApp.id)" -Method "GET"
+                        $Win32AppList.Add($Win32App) | Out-Null
+                    }
+                    
+                    # Handle return value
+                    return $Win32AppList
                 }
                 else {
-                    Write-Warning -Message "Query for Win32 apps returned empty a result, no apps matching the specified search criteria was found"
+                    Write-Warning -Message "Query for Win32 apps returned an empty result, no apps matching type 'win32LobApp' was found in tenant"
                 }
             }
-            else {
-                Write-Warning -Message "Query for Win32 apps returned empty a result, no apps matching type 'win32LobApp' was found in tenant"
-            }
-        }
-        else {
-            Write-Warning -Message "Query for mobileApps resources returned empty"
         }
     }
 }
