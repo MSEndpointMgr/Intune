753f31da2b57d1cf143d44cdac7970baa65d8292
Nickolaj Andersen <nickolaj@outlook.com>
2020-04-30 15:09:18 +0200

    1.2.0-RC

--- Modules/IntuneWin32App/IntuneWin32App.psm1
+++ Modules/IntuneWin32App/IntuneWin32App.psm1
@@ -610,7 +610,248 @@ function Add-IntuneWin32AppAssignment {
                 }
             }
             catch [System.Exception] {
-                Write-Warning -Message "An error occurred while creating a CryptoStream and writing decoded chunks of data to file: $($TargetFilePath). Error message: $($_.Exception.Message)"
+                Write-Warning -Message "An error occurred while creating a Win32 app assignment: $($TargetFilePath). Error message: $($_.Exception.Message)"
+            }
+        }
+        else {
+            Write-Warning -Message "Unable to determine the Win32 app identification for assignment"
+        }
+    }
+}
+
+function Get-IntuneWin32AppAssignment {
+    <#
+    .SYNOPSIS
+        Retrieve all assignments for a Win32 app.
+
+    .DESCRIPTION
+        Retrieve all assignments for a Win32 app.
+
+    .PARAMETER TenantName
+        Specify the tenant name, e.g. domain.onmicrosoft.com.
+
+    .PARAMETER DisplayName
+        Specify the display name for a Win32 application.
+
+    .PARAMETER ID
+        Specify the ID for a Win32 application.
+
+    .PARAMETER ApplicationID
+        Specify the Application ID of the app registration in Azure AD. By default, the script will attempt to use well known Microsoft Intune PowerShell app registration.
+
+    .PARAMETER PromptBehavior
+        Set the prompt behavior when acquiring a token.
+
+    .NOTES
+        Author:      Nickolaj Andersen
+        Contact:     @NickolajA
+        Created:     2020-04-29
+        Updated:     2020-04-29
+
+        Version history:
+        1.0.0 - (2020-04-29) Function created
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param(
+        [parameter(Mandatory = $true, ParameterSetName = "DisplayName", HelpMessage = "Specify the tenant name, e.g. domain.onmicrosoft.com.")]
+        [parameter(Mandatory = $true, ParameterSetName = "ID")]
+        [ValidateNotNullOrEmpty()]
+        [string]$TenantName,
+
+        [parameter(Mandatory = $true, ParameterSetName = "DisplayName", HelpMessage = "Specify the display name for a Win32 application.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$DisplayName,
+
+        [parameter(Mandatory = $true, ParameterSetName = "ID", HelpMessage = "Specify the ID for a Win32 application.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$ID,
+        
+        [parameter(Mandatory = $false, ParameterSetName = "DisplayName", HelpMessage = "Specify the Application ID of the app registration in Azure AD. By default, the script will attempt to use well known Microsoft Intune PowerShell app registration.")]
+        [parameter(Mandatory = $false, ParameterSetName = "ID")]
+        [ValidateNotNullOrEmpty()]
+        [string]$ApplicationID = "d1ddf0e4-d672-4dae-b554-9d5bdfd93547",
+    
+        [parameter(Mandatory = $false, ParameterSetName = "DisplayName", HelpMessage = "Set the prompt behavior when acquiring a token.")]
+        [parameter(Mandatory = $false, ParameterSetName = "ID")]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("Auto", "Always", "Never", "RefreshSession")]
+        [string]$PromptBehavior = "Auto"        
+    )
+    Begin {
+        # Ensure required auth token exists or retrieve a new one
+        Get-AuthToken -TenantName $TenantName -ApplicationID $ApplicationID -PromptBehavior $PromptBehavior
+    }
+    Process {
+        switch ($PSCmdlet.ParameterSetName) {
+            "DisplayName" {
+                $MobileApps = Invoke-IntuneGraphRequest -APIVersion "Beta" -Resource "mobileApps" -Method "GET"
+                if ($MobileApps.value.Count -ge 1) {
+                    $Win32MobileApps = $MobileApps.value | Where-Object { $_.'@odata.type' -like "#microsoft.graph.win32LobApp" }
+                    if ($Win32MobileApps -ne $null) {
+                        $Win32App = $Win32MobileApps | Where-Object { $_.displayName -like $DisplayName }
+                        if ($Win32App -ne $null) {
+                            Write-Verbose -Message "Detected Win32 app with ID: $($Win32App.id)"
+                            $Win32AppID = $Win32App.id
+                        }
+                        else {
+                            Write-Warning -Message "Query for Win32 apps returned empty a result, no apps matching the specified search criteria was found"
+                        }
+                    }
+                    else {
+                        Write-Warning -Message "Query for Win32 apps returned empty a result, no apps matching type 'win32LobApp' was found in tenant"
+                    }
+                }
+                else {
+                    Write-Warning -Message "Query for mobileApps resources returned empty"
+                }
+            }
+            "ID" {
+                $Win32AppID = $ID
+            }
+        }
+
+        if (-not([string]::IsNullOrEmpty($Win32AppID))) {
+            try {
+                # Attempt to call Graph and retrieve all assignments for Win32 app
+                $Win32AppAssignmentResponse = Invoke-IntuneGraphRequest -APIVersion "Beta" -Resource "mobileApps/$($Win32AppID)/assignments" -Method "GET" -ErrorAction Stop
+                if ($Win32AppAssignmentResponse.value -ne $null) {
+                    foreach ($Win32AppAssignment in $Win32AppAssignmentResponse.value) {
+                        Write-Verbose -Message "Successfully retrieved Win32 app assignment with ID: $($Win32AppAssignment.id)"
+                        Write-Output -InputObject $Win32AppAssignment
+                    }
+                }
+            }
+            catch [System.Exception] {
+                Write-Warning -Message "An error occurred while retrieving Win32 app assignments for app with ID: $($Win32AppID). Error message: $($_.Exception.Message)"
+            }
+        }
+        else {
+            Write-Warning -Message "Unable to determine the Win32 app identification for assignment"
+        }
+    }
+}
+
+function Remove-IntuneWin32AppAssignment {
+    <#
+    .SYNOPSIS
+        Remove all assignments for a Win32 app.
+
+    .DESCRIPTION
+        Remove all assignments for a Win32 app.
+
+    .PARAMETER TenantName
+        Specify the tenant name, e.g. domain.onmicrosoft.com.
+
+    .PARAMETER DisplayName
+        Specify the display name for a Win32 application.
+
+    .PARAMETER ID
+        Specify the ID for a Win32 application.
+
+    .PARAMETER ApplicationID
+        Specify the Application ID of the app registration in Azure AD. By default, the script will attempt to use well known Microsoft Intune PowerShell app registration.
+
+    .PARAMETER PromptBehavior
+        Set the prompt behavior when acquiring a token.
+
+    .NOTES
+        Author:      Nickolaj Andersen
+        Contact:     @NickolajA
+        Created:     2020-04-29
+        Updated:     2020-04-29
+
+        Version history:
+        1.0.0 - (2020-04-29) Function created
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param(
+        [parameter(Mandatory = $true, ParameterSetName = "DisplayName", HelpMessage = "Specify the tenant name, e.g. domain.onmicrosoft.com.")]
+        [parameter(Mandatory = $true, ParameterSetName = "ID")]
+        [ValidateNotNullOrEmpty()]
+        [string]$TenantName,
+
+        [parameter(Mandatory = $true, ParameterSetName = "DisplayName", HelpMessage = "Specify the display name for a Win32 application.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$DisplayName,
+
+        [parameter(Mandatory = $true, ParameterSetName = "ID", HelpMessage = "Specify the ID for a Win32 application.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$ID,
+        
+        [parameter(Mandatory = $false, ParameterSetName = "DisplayName", HelpMessage = "Specify the Application ID of the app registration in Azure AD. By default, the script will attempt to use well known Microsoft Intune PowerShell app registration.")]
+        [parameter(Mandatory = $false, ParameterSetName = "ID")]
+        [ValidateNotNullOrEmpty()]
+        [string]$ApplicationID = "d1ddf0e4-d672-4dae-b554-9d5bdfd93547",
+    
+        [parameter(Mandatory = $false, ParameterSetName = "DisplayName", HelpMessage = "Set the prompt behavior when acquiring a token.")]
+        [parameter(Mandatory = $false, ParameterSetName = "ID")]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("Auto", "Always", "Never", "RefreshSession")]
+        [string]$PromptBehavior = "Auto"        
+    )
+    Begin {
+        # Ensure required auth token exists or retrieve a new one
+        Get-AuthToken -TenantName $TenantName -ApplicationID $ApplicationID -PromptBehavior $PromptBehavior
+    }
+    Process {
+        switch ($PSCmdlet.ParameterSetName) {
+            "DisplayName" {
+                $MobileApps = Invoke-IntuneGraphRequest -APIVersion "Beta" -Resource "mobileApps" -Method "GET"
+                if ($MobileApps.value.Count -ge 1) {
+                    $Win32MobileApps = $MobileApps.value | Where-Object { $_.'@odata.type' -like "#microsoft.graph.win32LobApp" }
+                    if ($Win32MobileApps -ne $null) {
+                        $Win32App = $Win32MobileApps | Where-Object { $_.displayName -like $DisplayName }
+                        if ($Win32App -ne $null) {
+                            Write-Verbose -Message "Detected Win32 app with ID: $($Win32App.id)"
+                            $Win32AppID = $Win32App.id
+                        }
+                        else {
+                            Write-Warning -Message "Query for Win32 apps returned empty a result, no apps matching the specified search criteria was found"
+                        }
+                    }
+                    else {
+                        Write-Warning -Message "Query for Win32 apps returned empty a result, no apps matching type 'win32LobApp' was found in tenant"
+                    }
+                }
+                else {
+                    Write-Warning -Message "Query for mobileApps resources returned empty"
+                }
+            }
+            "ID" {
+                $Win32AppID = $ID
+            }
+        }
+
+        if (-not([string]::IsNullOrEmpty($Win32AppID))) {
+            try {
+                # Attempt to call Graph and retrieve all assignments for Win32 app
+                $Win32AppAssignmentResponse = Invoke-IntuneGraphRequest -APIVersion "Beta" -Resource "mobileApps/$($Win32AppID)/assignments" -Method "GET" -ErrorAction Stop
+                if ($Win32AppAssignmentResponse.value -ne $null) {
+                    Write-Verbose -Message "Count of assignments for Win32 app before attempted removal process: $(($Win32AppAssignmentResponse.value | Measure-Object).Count)"
+
+                    # Process each assignment for removal
+                    foreach ($Win32AppAssignment in $Win32AppAssignmentResponse.value) {
+                        Write-Verbose -Message "Attempting to remove Win32 app assignment with ID: $($Win32AppAssignment.id)"
+                        
+                        try {
+                            # Remove current assignment
+                            $Win32AppAssignmentRemoveResponse = Invoke-IntuneGraphRequest -APIVersion "Beta" -Resource "mobileApps/$($Win32AppID)/assignments/$($Win32AppAssignment.id)" -Method "DELETE" -ErrorAction Stop
+                        }
+                        catch [System.Exception] {
+                            Write-Warning -Message "An error occurred while retrieving Win32 app assignments for app with ID: $($Win32AppID). Error message: $($_.Exception.Message)"
+                        }
+                    }
+
+                    # Calculate amount of remaining assignments after attempted removal process
+                    $Win32AppAssignmentResponse = Invoke-IntuneGraphRequest -APIVersion "Beta" -Resource "mobileApps/$($Win32AppID)/assignments" -Method "GET" -ErrorAction Stop
+                    Write-Verbose -Message "Count of assignments for Win32 app after attempted removal process: $(($Win32AppAssignmentResponse.value | Measure-Object).Count)"
+                }
+                else {
+                    Write-Verbose -Message "Unable to locate any instances for removeal, Win32 app does not have any existing assignments"
+                }
+            }
+            catch [System.Exception] {
+                Write-Warning -Message "An error occurred while retrieving Win32 app assignments for app with ID: $($Win32AppID). Error message: $($_.Exception.Message)"
             }
         }
         else {
@@ -1631,10 +1872,11 @@ function Invoke-IntuneGraphRequest {
         Author:      Nickolaj Andersen
         Contact:     @NickolajA
         Created:     2020-01-04
-        Updated:     2020-01-04
+        Updated:     2020-04-29
 
         Version history:
         1.0.0 - (2020-01-04) Function created
+        1.0.1 - (2020-04-29) Added support for DELETE operations
     #>    
     param(
         [parameter(Mandatory = $true)]
@@ -1648,7 +1890,7 @@ function Invoke-IntuneGraphRequest {
 
         [parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
-        [ValidateSet("GET", "POST", "PATCH")]
+        [ValidateSet("GET", "POST", "PATCH", "DELETE")]
         [string]$Method,
 
         [parameter(Mandatory = $false)]
@@ -1675,6 +1917,9 @@ function Invoke-IntuneGraphRequest {
             }
             "PATCH" {
                 $GraphResponse = Invoke-RestMethod -Uri $GraphURI -Headers $AuthToken -Method $Method -Body $Body -ContentType $ContentType -ErrorAction Stop -Verbose:$false
+            }
+            "DELETE" {
+                $GraphResponse = Invoke-RestMethod -Uri $GraphURI -Headers $AuthToken -Method $Method -ErrorAction Stop -Verbose:$false
             }
         }
 
