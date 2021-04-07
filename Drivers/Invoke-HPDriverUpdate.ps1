105b29cf7b48cccb5a6af8f429f9db3a4d94c16f
brucesa85 <36322885+brucesa85@users.noreply.github.com>
2021-04-06 23:51:01 -0400

    Update Invoke-HPDriverUpdate.ps1
    added Install-HPImageAssistant

--- Drivers/Invoke-HPDriverUpdate.ps1
+++ Drivers/Invoke-HPDriverUpdate.ps1
@@ -29,6 +29,7 @@
     1.0.0 - (2020-08-12) Script created
     1.0.1 - (2020-09-15) Added a fix for registering default PSGallery repository if not already registered
     1.0.2 - (2020-09-28) Added a new parameter HPIAAction that controls whether to Download or Install applicable drivers
+    1.0.3 - (2021-04-06) Replaced HPIA Softpak with new Install-HPImageAssistant function
 #>
 [CmdletBinding(SupportsShouldProcess = $true)]
 param(
@@ -36,10 +37,6 @@ param(
     [ValidateNotNullOrEmpty()]
     [ValidateSet("Stage", "Execute")]
     [string]$RunMode,
-
-    [parameter(Mandatory = $false, HelpMessage = "Specify the HP Image Assistant softpaq number.")]
-    [ValidateNotNullOrEmpty()]
-    [string]$HPIASoftpaqNumber = "sp103654",
 
     [parameter(Mandatory = $false, HelpMessage = "Specify the HP Image Assistant action to perform, e.g. Download or Install.")]
     [ValidateNotNullOrEmpty()]
@@ -335,7 +332,7 @@ Process {
                     try {
                         # Download HP Image Assistant softpaq and extract it to Temp directory
                         Write-LogEntry -Value "Attempting to download and extract HP Image Assistant to: $($HPImageAssistantExtractPath)" -Severity 1
-                        Get-Softpaq -Number $HPIASoftpaqNumber -Extract -DestinationPath $HPImageAssistantExtractPath -Overwrite "yes" -Verbose -ErrorAction Stop
+                        Install-HPImageAssistant -DestinationPath $HPImageAssistantExtractPath -Extract -Quiet -Verbose -ErrorAction Stop
     
                         try {
                             # Invoke HP Image Assistant to install drivers and driver software
