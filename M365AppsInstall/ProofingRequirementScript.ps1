9fc624254e276ab1aa98434f3c9ff61d094ff943
Jan Ketil Skanke [MVP] <jan.ketil@outlook.com>
2022-10-23 19:44:57 +0200

    Move

--- M365AppsInstall/ProofingRequirementScript.ps1
+++
@@ -1,7 +0,0 @@
-$RegistryKeys = Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
-$M365Apps = "Microsoft 365 Apps"
-$M365AppsCheck = $RegistryKeys | Get-ItemProperty | Where-Object { $_.DisplayName -match $M365Apps }
-if ($M365AppsCheck) {
-    Write-Output "OK"
-	Exit 0
-   }
